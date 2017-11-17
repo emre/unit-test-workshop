6a323dd7ffac08e7aa56e09d307798d4ae984fa9
Emre Yilmaz <emre.yilmaz@metglobal.com>
2017-11-17 15:30:07 +0300

    Implement calculator

--- case3/calc.py
+++ case3/calc.py
@@ -1,21 +1,23 @@
 
+import math
+
 class Calc:
 
     def add(self, a, b):
-        pass
+        return a + b
 
     def substract(self, a, b):
-        pass
+        return a - b
 
     def multiply(self, a, b):
-        pass
+        return a * b
 
     def divide(self, a, b):
-        pass
+        return a / b
 
     def factorial(self, number):
-        pass
+        return math.factorial(number)
 
     def sqrt(self, number):
-        pass
+        return math.sqrt(number)
 
