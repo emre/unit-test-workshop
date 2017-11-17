7bd9f3233e7dc1074d546bfc7ffed57f25e254ad
Emre Yilmaz <emre.yilmaz@metglobal.com>
2017-11-17 15:28:12 +0300

    Update palindrome implementation and its tests

--- case2/is_palindrome.py
+++ case2/is_palindrome.py
@@ -1,6 +1,9 @@
+import re
+
 
 def is_palindrome(text):
+    text = re.sub('[^a-zA-Z]', '', text)
+    text = text.lower()
     return text == text[::-1]
-
 
 
--- case2/test_is_palindrome.py
+++ case2/test_is_palindrome.py
@@ -1,7 +1,32 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 
 from is_palindrome import is_palindrome
 
 
 class TestIsPalindrome(unittest.TestCase):
-    pass
+
+    def test_success(self):
+        self.assertEqual(is_palindrome("kacak"), True)
+
+    def test_failure(self):
+        self.assertEqual(is_palindrome("caner"), False)
+
+    def test_sentence(self):
+        self.assertEqual(
+            is_palindrome("At, sahibi gibi hasta."), True)
+
+    def test_integers(self):
+        with self.assertRaises(TypeError):
+            is_palindrome(123)
+
+    def test_integers_as_string(self):
+        self.assertEqual(is_palindrome("123123"), True)
+
+    def test_upper_i(self):
+        self.assertEqual(is_palindrome("İKİ"), True)
+        self.assertEqual(is_palindrome("IKI"), True)
+
+
+unittest.main()
